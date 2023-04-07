be85282db50697b7ad60573a94c4feca0818e798
Gregory Williams <gregwills85@gmail.com>
2023-04-07 17:05:23 -0600

    Added fonts, fixed man page

--- config.def.h
+++ config.def.h
@@ -3,13 +3,18 @@
 static int barheight			= 5;		/* bar height in pixels */
 static int topbar				= 1;		/* -b option */
 static int borderpx				= 5;		/* -bw option */
+static int centered				= 1;		/* -c option */
+static int min_width			= 500;		/* min width when centered */
 static unsigned int lines		= 5;		/* -l option  */
 static const char *prompt		= NULL;		/* -p option */
-static const unsigned int alpha = 0xf0;		/* alpha value */
+static const unsigned int alpha = 0xFA;		/* alpha value */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
+static const char *fonts[] = { "Liberation Mono:style=Regular:size=10:antialias=true:autohint=true",
+							   "JoyPixels:style=Regular:size=10:antialias=true:autohint=true",
+							   "Font Awesome 5 Free Regular:style=Regular:size=10:antialias=true:autohint=true",
+							   "Font Awesome 5 Free Solid:style=Solid:size=10:antialias=true:autohint=true",
+							   "Font Awesome 5 Brands Regular:style=Regular:size=10:antialias=true:autohint=true"
 };
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
